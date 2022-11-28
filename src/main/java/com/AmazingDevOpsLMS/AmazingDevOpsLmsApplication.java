2cc9894b29290e11296b76e0a91325a7f99539d5
samuel-maina <maina9025@gmail.com>
2022-11-28 08:31:05 +0300

    initial commit

--- src/main/java/com/AmazingDevOpsLMS/AmazingDevOpsLmsApplication.java
+++ src/main/java/com/AmazingDevOpsLMS/AmazingDevOpsLmsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AmazingDevOpsLmsApplication {
 
 	public static void main(String[] args) {
+            String m="";
 		SpringApplication.run(AmazingDevOpsLmsApplication.class, args);
 	}
 
